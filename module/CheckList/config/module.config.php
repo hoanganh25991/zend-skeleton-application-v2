e1c7c5121ed19212488e179a1eacd6f2baa200e6
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-04-06 10:01:52 +0700

    Zend\View, addChild to $viewModel

--- module/CheckList/config/module.config.php
+++ module/CheckList/config/module.config.php
@@ -1,5 +1,15 @@
 <?php
 return array(
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=zend_skeleton_task_item;hostname=localhost',
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+    ),
     'controllers' => array(
         'invokables' => array(
             'Task' => 'CheckList\Controller\TaskController',
@@ -12,7 +22,7 @@ return array(
                 'options' => array(
                     'route' => '/task[/:action[/:id]]',
                     'defaults' => array(
-//                        '__NAMESPACE__' => 'Checklist\Controller',
+                        //                        '__NAMESPACE__' => 'Checklist\Controller',
                         'controller' => 'Task',
                         'action' => 'index',
                     ),
@@ -24,7 +34,11 @@ return array(
             ),
         ),
     ),
-    'view_manager' =>array(
+    'view_manager' => array(
+        'template_map' => array(
+            'avatar' => __DIR__.'/../view/check-list/avatar.phtml',
+
+        ),
         'template_path_stack' => array(
             'album' => __DIR__ . '/../view',
         ),
